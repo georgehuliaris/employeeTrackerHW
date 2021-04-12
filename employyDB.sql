1b9207885e63138af269905ae5dc35bf36cea98f
George Huliaris <georgehuliaris@me.com>
2021-04-12 16:56:02 -0500

    added to DB

--- employyDB.sql
+++ employyDB.sql
@@ -1,22 +1,23 @@
+DROP DATABASE IF EXISTS employyDB;
 CREATE DATABASE employyDB;
 
 USE employyDB;
 
 CREATE TABLE department(
-	id INT NOT NULL
+	id INT NOT NULL AUTO_INCREMENT
     , name VARCHAR(30) NOT NULL
 , PRIMARY KEY(id)
 );
 CREATE TABLE role(
 	id INT NOT NULL
     , title VARCHAR(30) NULL
-    , salary DECIMAL NOT NULL
+    , salary DECIMAL (10, 2) default 0
     , department_id INT NULL
 , PRIMARY KEY(id)
 );
 
 CREATE TABLE employee(
-	id INT NOT NULL
+	id INT NOT NULL AUTO_INCREMENT
     , first_name VARCHAR(30) NULL
     , role_id INT NULL
     , manager_id INT NULL
