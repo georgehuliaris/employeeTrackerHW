7feeb830d484742bc8247e6546d829b2c27c167c
George Huliaris <georgehuliaris@me.com>
2021-04-12 17:55:32 -0500

    question are working

--- employyDB.sql
+++ employyDB.sql
@@ -18,7 +18,7 @@ CREATE TABLE role(
 
 CREATE TABLE employee(
 	id INT NOT NULL AUTO_INCREMENT
-    , first_name VARCHAR(30) NULL
+    , first_name VARCHAR(30) NOT NULL
     , role_id INT NULL
     , manager_id INT NULL
     , department_id INT NULL
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    // Your port, if not 3306
-    port: 3300,
-    user: 'root',
-    password: 'BootCamp',
-    database: 'employyDB',
-  });
-  
-  connection.connect((err) => {
-    if (err) throw err;
-    console.log(`connected as id ${connection.threadId}`);
-    connection.end();
-  });
+  host: "localhost",
+  // Your port, if not 3306
+  port: 3306,
+  user: "root",
+  password: "BootCamp",
+  database: "employydb",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}`);
+  connection.end();
+});
 
 inquirer
   .prompt([
@@ -34,38 +35,38 @@ inquirer
       message: "What is your current annual salary?",
     },
     {
-        type: 'input',
-        name: 'department_id',
-        message: 'What department do you work in?',
-      },
-      {
-        type: 'input',
-        name: 'first_name',
-        message: 'What is your first name?',
-      },
-      {
-        type: 'input',
-        name: 'last_name',
-        message: 'What is your last name?',
-      },
-      {
-        type: 'input',
-        name: 'role_id',
-        message: 'What is your role id?',
-      },
-      {
-        type: 'input',
-        name: 'manager_id',
-        message: "What is your manager's name?",
-      },
+      type: "input",
+      name: "department_id",
+      message: "What department do you work in?",
+    },
+    {
+      type: "input",
+      name: "first_name",
+      message: "What is your first name?",
+    },
+    {
+      type: "input",
+      name: "last_name",
+      message: "What is your last name?",
+    },
+    {
+      type: "input",
+      name: "role_id",
+      message: "What is your role id?",
+    },
+    {
+      type: "input",
+      name: "manager_id",
+      message: "What is your manager's name?",
+    },
   ])
   .then((data) => {
-    const filename = `${data.name.toLowerCase().split(" ").join("")}.json`;
-
-    fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
-      err ? console.log(err) : console.log("Success!")
-    );
+    const filename = `${data.toLowerCase().split(" ").join("")}.json`;
+    console.log(data);
+    //     fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
+    //       err ? console.log(err) : console.log("Success!")
+    //     );
   });
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // still need to write answers to file
